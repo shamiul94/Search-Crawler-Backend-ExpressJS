71d8b00f64b4d1fe2ee3e34fe06d734146e7740d
shamiul94 <1505038.sh@ugrad.cse.buet.ac.bd>
2021-02-15 20:37:19 +0600

    scraper added in controller folder

--- collector_app/routes/index.js
+++ collector_app/routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+const ScraperController = require('../controllers/ScraperController');
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
@@ -13,5 +16,9 @@ router.get("/url", (req, res, next) => {
 router.get("/abc", (req, res, next) => {
     res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
 });
+
+
+router.get('/crawldata', ScraperController.scraper);
+
 
 module.exports = router;
